bc5472d8efe960a63b7b404bdfdb11b90e554a6a
Geonhui JO <dev.ghuijo@gmail.com>
2022-03-11 17:57:54 +0900

    요소 제거 코드 변경

--- code/bag-of-words.py
+++ code/bag-of-words.py
@@ -1,13 +1,23 @@
 from konlpy.tag import Okt
 from konlpy.tag import Kkma
-
+import re
 okt = Okt()
 kkma = Kkma()
 
+workspace = "E:/workspace/hashtag-classifier/"
+
+def remove_unnecessary(document):
+      document = re.sub(r'[\t\r\n\f\v]', ' ', str(document))
+      document = re.sub('[^ ㄱ-ㅣ 가-힣]+', ' ', str(document))
+      return document
+
 def build_bag_of_words(document):
-  # 온점 제거 및 형태소 분석
+  # 온점과 불필요한 요소 제거
   document = document.replace('.', '')
-  #tokenized_document = okt.morphs(document)
+  document = remove_unnecessary(document)
+  
+  # 형태소 분석
+  # tokenized_document = okt.morphs(document)
   tokenized_document = kkma.morphs(document)
 
   word_to_index = {}
@@ -16,9 +26,15 @@ def build_bag_of_words(document):
                    "등", "적극", "대신", "와의", "의", "했다", "별", "약", "이상", "이외", "이하", "미만", "초과", "포함", "모든", "모두",
                    "부터", "까지", "되나", "도", "이미", "된", "한", "된다", "될", "해야", "한다", "할", "다", "이다", "있다", "위해", "위하",
                    "여", "이번", "것", "명", "일", "하", "ㄴ", "아요", "요", "ㄴ다", "세", "ㄹ", "되", "들", "있", "며", "단", "었", "어",
-                   "두", "년", "시", "원", "수", "도록", "기", "지", "면", "연", "만", "간", "더", "그동안", "겠", "通", "아", "오", "으니", "니",
-                   "...", "['", "']", "^", "“", "”", ",", "‘", "’", "△", "▲", "◇", "[", "]", "(", ")", "·", "~", "%", "?", ":", "-", "!",
-                   "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+                   "두", "년", "시", "원", "수", "도록", "기", "지", "면", "연", "만", "간", "더", "그동안", "겠", "아", "오", "으니", "니",]
+  
+  # word_excluded2 = ["하는", "은", "는", "이", "가", "와", "과", "으로", "로", "을", "를", "고", "이나", "적", "에", "게", "께", "에게", "에서",
+  #                 "등", "적극", "대신", "와의", "의", "했다", "별", "약", "이상", "이외", "이하", "미만", "초과", "포함", "모든", "모두",
+  #                 "부터", "까지", "되나", "도", "이미", "된", "한", "된다", "될", "해야", "한다", "할", "다", "이다", "있다", "위해", "위하",
+  #                 "여", "이번", "것", "명", "일", "하", "ㄴ", "아요", "요", "ㄴ다", "세", "ㄹ", "되", "들", "있", "며", "단", "었", "어",
+  #                 "두", "년", "시", "원", "수", "도록", "기", "지", "면", "연", "만", "간", "더", "그동안", "겠", "通", "아", "오", "으니", "니",
+  #                 "...", "['", "']", "^", "“", "”", ",", "‘", "’", "△", "▲", "◇", "[", "]", "(", ")", "·", "~", "%", "?", ":", "-", "!",
+  #                 "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
   for word in tokenized_document:  
     if word not in word_to_index.keys():
@@ -44,12 +60,15 @@ def build_bag_of_words(document):
 
 import pandas as pd
 
-docs = pd.read_csv("../data/train01.csv", encoding="UTF-8")
+docs = pd.read_csv(workspace + "data/train01.csv", encoding="UTF-8")
 total = pd.DataFrame()
 j = 0
 
 for i in docs['article'].unique():
-    if (j >= 10000): break
+    if (j < 800):
+          j += 1
+          continue
+    if (j >= 1000): break
     article = i
     doc = article
     vocab, bow, word = build_bag_of_words(doc)
@@ -63,4 +82,4 @@ for i in docs['article'].unique():
     j += 1
     print(j, "added")
     
-total.to_csv("../result/bows01.csv", index=False, encoding="utf-8-sig")
+total.to_csv(workspace + "data/bows01_test.csv", index=False, encoding="utf-8-sig")
